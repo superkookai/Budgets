9e369df91c89e5f4c5073eb7a2a75b5fcb4f6d16
superkookai <weerawut@live.com>
2025-03-05 10:49:28 +0700

    Initial Commit

---
+++ Budgets/BudgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetsApp.swift
+//  Budgets
+//
+//  Created by Weerawut Chaiyasomboon on 05/03/2568.
+//
+
+import SwiftUI
+
+@main
+struct BudgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
